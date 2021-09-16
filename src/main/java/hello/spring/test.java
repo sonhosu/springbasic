ff397c0feda0c7eaf11c5fd0f37468e012490147
sonhosu <ghtn9398@nate.com>
2021-09-16 16:39:49 +0900

    정적컨텐츠,MVC와 템플릿엔진 ,API 설명

---
+++ src/main/java/hello/spring/test.java
@@ -0,0 +1,14 @@
+package hello.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class test {
+
+    public static void main(String[] args) {
+        SpringApplication.run(test.class, args);
+    }
+
+}
+
